3082c338fd0b8d601682056f395bd358771a251e
Júlio César Fragoso de Lima <103080100+chakaartt@users.noreply.github.com>
2022-09-21 19:31:30 -0300

    finalizado_exercicio1

---
+++ aula1_exercicio1/exercicio1.js
@@ -0,0 +1,26 @@
+// alert("JS is Awesome!");-----> o alert como o nome msm diz, criar um alert no brows ao carregar.
+
+
+// const é uma variável que ao atribuir um valor a ela, esse valor não podera se alterado.
+// então, a const é mais usada pra valores onde vai permanece o msm durante todo o código.
+
+
+
+const myBirthYear = 1986;
+
+const currentYear = 2022;
+
+
+console.log(myBirthYear, currentYear);
+
+
+const myAge = myBirthYear - currentYear;
+
+// console.log " ele imprime no console do brows."
+
+console.log(myAge);
+
+const ten = 10;
+
+
+console.log(`Em ${currentYear + ten}, estarei com ${myAge + ten} anos.`)
